b35767e22ec6f3d252766ff7651e9ead7fc7fe55
Csányi István <csanyistvan02@gmail.com>
2019-07-06 13:39:31 +0200

    replace necessary with main->result

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,17 @@ mod watcher;
 
 use config::Config;
 use crossbeam_utils::sync::ShardedLock;
-use std::any::Any;
-use std::fmt::Display;
+use std::error::Error;
 use std::sync::Arc;
 
 type SharedConfig = Arc<ShardedLock<Config>>;
 
-fn main() {
-    let cfg = necessary(config::load(config::path()));
-    necessary(config::verify(&cfg));
+fn main() -> Result<(), Box<dyn Error>> {
+    let cfg = config::load(config::path())?;
+    config::verify(&cfg)?;
 
     let cfg = Arc::new(ShardedLock::new(cfg));
-    let _watcher = necessary(watcher::watch(cfg.clone()));
+    let _watcher = watcher::watch(cfg.clone())?;
 
-    necessary(ignite::start(cfg));
-}
-
-fn necessary<T: Any, E: Display>(result: Result<T, E>) -> T {
-    match result {
-        Ok(value) => value,
-        Err(error) => {
-            eprintln!("Fatal Error: {}", error);
-            std::process::exit(1);
-        }
-    }
+    ignite::start(cfg)
 }
